ca75045bdfb164823ee6674d7c3694f9888d3189
Vanda89 <sandrinealcazar@gmail.com>
2024-05-24 15:49:45 +0200

    chore: Update vite configuration for polling and proxy settings

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    watch: {
+      usePolling: true,
+      interval: 80,
+    },
+    proxy: {
+      '/api': 'http://localhost:3001',
+    },
+  },
 })
