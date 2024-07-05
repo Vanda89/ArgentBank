1d23c42bf66b49dce81ecd095d1211b0686c3ac8
Vanda89 <sandrinealcazar@gmail.com>
2024-07-05 19:09:20 +0200

    feat: Add NotFound content

--- client/src/pages/NotFound/index.jsx
+++ client/src/pages/NotFound/index.jsx
@@ -1,3 +1,15 @@
+import './NotFound.css'
+import { NavLink } from 'react-router-dom'
+import { ROUTES } from '../../config'
+
 export default function NotFound() {
-  return <></>
+  return (
+    <main className="main bg-dark notFound">
+      <h1>Page Not Found</h1>
+      <p>The page you are looking for does not exist.</p>
+      <NavLink className="home-link" to={ROUTES.HOME}>
+        <button alt="Return button to the home page">Back to Home</button>
+      </NavLink>
+    </main>
+  )
 }
